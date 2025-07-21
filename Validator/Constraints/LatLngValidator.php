838211184bce476a5b9f1feb4b8c715d150fba00
Tom Borealis <tom@lampbristol.com>
2025-07-21 22:46:22 +0100

    Remove boolean return types in validator

--- Validator/Constraints/LatLngValidator.php
+++ Validator/Constraints/LatLngValidator.php
@@ -7,21 +7,18 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class LatLngValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint)
+    public function validate($value, Constraint $constraint): void
     {
         $latitude = isset($value['latitude']) ? $value['latitude'] : $value['lat'];
         $longitude = isset($value['longitude']) ? $value['longitude'] : $value['lng'];
 
         if (!preg_match('/^[0-9\-\.]+$/', $latitude, $matches) || !preg_match('/^[0-9\-\.]+$/', $longitude, $matches)) {
             $this->context->addViolation($constraint->message, array('%latitude%' => (float)$latitude, '%longitude%' => (float)$longitude));
-            return false;
         }
+
         if($latitude > 90 || $latitude < -90 || $longitude > 180 || $longitude < -180)
         {
             $this->context->addViolation($constraint->message, array('%latitude%' => (float)$latitude, '%longitude%' => (float)$longitude));
-            return false;
         }
-
-        return true;
     }
 }
